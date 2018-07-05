86a7d617aabe5163d96b4737375fdc2dde0fa2fb
heng_sir <mpneugg1314@foxmail.com>
2018-07-05 10:29:03 +0800

    新增：https

--- src/main/java/org/hengsir/simpleBlogComment/Main.java
+++ src/main/java/org/hengsir/simpleBlogComment/Main.java
@@ -1,10 +1,15 @@
 package org.hengsir.simpleBlogComment;
 
+import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,6 +22,9 @@ import java.util.Date;
 @MapperScan("org.hengsir.simpleBlogComment.dao.mapper")
 public class Main{
 
+    @Value("${http.port}")
+    private int port;
+
    /* @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Main.class);
@@ -25,5 +33,21 @@ public class Main{
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
         System.out.println("-----------------嘤嘤嘤嘤嘤~~~~启动成功！！！！！------------------" + new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()));
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    // 配置http
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(port);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
     }
 }
