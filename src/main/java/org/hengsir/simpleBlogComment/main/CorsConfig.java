a116b8621e460dd68bd2223e4fc0214e8f0e8767
heng_sir <mpneugg1314@foxmail.com>
2018-07-05 09:49:09 +0800

    尝试跨域

---
+++ src/main/java/org/hengsir/simpleBlogComment/main/CorsConfig.java
@@ -0,0 +1,29 @@
+package org.hengsir.simpleBlogComment.main;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author hengsir
+ * @date 2018/7/5 上午9:02
+ */
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
