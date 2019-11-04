37284a9d336fb47e7cc8eee24a3b84ca60477ca3
heng_sir <mpneugg1314@foxmail.com>
2019-11-04 19:41:52 +0800

    新增小说功能

--- src/main/java/org/hengsir/websocket/DefaultChannelInitializer.java
+++ src/main/java/org/hengsir/websocket/DefaultChannelInitializer.java
@@ -6,12 +6,19 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 import lombok.Data;
 import org.hengsir.websocket.handler.WebSocketHandler;
+import org.springframework.core.io.ClassPathResource;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
@@ -32,7 +39,7 @@ public class DefaultChannelInitializer extends ChannelInitializer<SocketChannel>
                CertificateException, UnrecoverableKeyException {
         ChannelPipeline pipeline = channel.pipeline();
         // 以下为要支持wss所需处理
-        /*KeyStore ks = KeyStore.getInstance("JKS");
+        KeyStore ks = KeyStore.getInstance("JKS");
         ClassPathResource cpr = new ClassPathResource("www.hengsir.cn.jks");
         InputStream ksInputStream = cpr.getInputStream();
         ks.load(ksInputStream, sslPwd.toCharArray());
@@ -44,7 +51,7 @@ public class DefaultChannelInitializer extends ChannelInitializer<SocketChannel>
         sslEngine.setUseClientMode(false);
         sslEngine.setNeedClientAuth(false);
         // 需把SslHandler添加在第一位
-        pipeline.addLast("ssl", new SslHandler(sslEngine));*/
+        pipeline.addLast("ssl", new SslHandler(sslEngine));
         //websocket协议本身是基于http协议的，所以这边也要使用http解编码器
         pipeline.addLast("http-codec",new HttpServerCodec());
         //netty是基于分段请求的，HttpObjectAggregator的作用是将请求分段再聚合,参数是聚合字节的最大长度
